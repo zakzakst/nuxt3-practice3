2ed076ce8fdc17ef7be1be45ea46c7a115bc2dea
zakzakst <e1433104@gmail.com>
2022-11-06 12:26:02 +0900

    base files

--- section6/app.vue
+++ section6/app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <Nav />
+    <Heading />
+    <Cards />
   </div>
 </template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  font-family: Georgia;
+}
+</style>
