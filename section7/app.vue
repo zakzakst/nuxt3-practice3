8a1842b840522ca8dfbc98998e749f2243f02fa5
zakzakst <e1433104@gmail.com>
2022-11-10 16:24:24 +0900

    section7

--- section7/app.vue
+++ section7/app.vue
@@ -1,33 +1,56 @@
 <script setup lang="ts">
-const search = ref('Toronto');
+const cookie = useCookie('city');
+const config = useRuntimeConfig();
+
+console.log({ config });
+if (!cookie.value) cookie.value = 'Toronto';
+
+const search = ref(cookie.value);
 const input = ref('');
 const background = ref('');
 
-// const {data: city, error} = useFetch(
-//   () => `https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=`
-// );
-
-const { data: city, error } = useAsyncData('city', async () => {
-  const response = await $fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=`);
-
-  const temp = response.main.temp;
-  if (temp <= -10) {
-    background.value =
-      "https://images.unsplash.com/photo-1483664852095-d6cc6870702d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
-  } else if (temp > -10 && temp <= 0) {
-    background.value =
-      "https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
-  } else if (temp > 0 && temp <= 10) {
-    background.value =
-      "https://images.unsplash.com/photo-1560258018-c7db7645254e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4032&q=80";
-  } else {
-    background.value =
-      "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3546&q=80";
+const { data: city, error } = useAsyncData(
+  'city',
+  async () => {
+    let response;
+    try {
+      response = await $fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${search.value}`,
+        {
+          params: {
+            units: 'metric',
+            appid: config.WEATHER_APP_SECRET,
+          }
+        }
+      );
+      cookie.value = search.value;
+      const temp = response.main.temp;
+      if (temp <= -10) {
+        background.value =
+          "https://images.unsplash.com/photo-1483664852095-d6cc6870702d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
+      } else if (temp > -10 && temp <= 0) {
+        background.value =
+          "https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
+      } else if (temp > 0 && temp <= 10) {
+        background.value =
+          "https://images.unsplash.com/photo-1560258018-c7db7645254e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4032&q=80";
+      } else {
+        background.value =
+          "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3546&q=80";
+      }
+    } catch(e) {}
+    return response;
+  }, {
+    watch: [search],
   }
+);
 
-  return response;
-}, {
-  watch: [search],
+let today = new Date();
+today = today.toLocaleDateString('en-US', {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
 });
 
 const handleClick = () => {
@@ -35,17 +58,21 @@ const handleClick = () => {
   search.value = formattedSearch;
   input.value = '';
 }
+
+const goBack = () => {
+  search.value = cookie.value;
+}
 </script>
 
 <template>
-  <div class="h-screen relative overflow-hidden">
+  <div v-if="city" class="h-screen relative overflow-hidden">
     <img :src="background" alt="">
     <div class="absolute w-full h-full top-0 overlay" />
     <div class="absolute w-full h-full top-0 p-48">
       <div class="flex justify-between">
         <div>
           <h1 class="text-7xl text-white">{{ city.name }}</h1>
-          <p class="font-extralight text-2xl mt-2 text-white">Sunday Dec 9th</p>
+          <p class="font-extralight text-2xl mt-2 text-white">{{ today }}</p>
           <img :src="`https://openweathermap.org/img/wn/${ city.weather[0].icon }@4x.png`" alt="" class="w-56 icon">
         </div>
         <div>
@@ -57,6 +84,10 @@ const handleClick = () => {
         <button class="bg-sky-400 w-20 text-white h-10" @click="handleClick">Search</button>
       </div>
     </div>
+  </div>
+  <div v-else class="p-10">
+    <h1 class="text-7xl">Oops, we can't find that city</h1>
+    <button class="mt-5 bg-sky-400 px-10 w-50 text-white h-10" @click="goBack">Go Back</button>
   </div>
 </template>
 
--- section7/nuxt.config.ts
+++ section7/nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  publicRuntimeConfig: {
+    WEATHER_APP_SECRET: process.env.WEATHER_APP_SECRET,
+  },
+  privateRuntimeConfig: {
+    HELLO: 'world in the server not the client'
+  },
   // meta: {
   //   link: [
   //     {
