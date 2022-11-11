8b5261517f50baf8a498c5982ca557359b9bc012
zakzakst <e1433104@gmail.com>
2022-11-11 12:04:02 +0900

    section9

---
+++ section9/server/api/todo/index.ts
@@ -0,0 +1,23 @@
+import { db } from '../../db';
+import { v4 as uuid } from 'uuid';
+
+export default defineEventHandler(async (e) => {
+  const method = e.req.method;
+
+  if (method === 'GET') {
+    return db.todos;
+  }
+
+  if (method === 'POST') {
+    const body = await useBody(e);
+    if (!body.item) throw new Error();
+    const newTodo = {
+      id: uuid(),
+      item: body.item,
+      computed: false,
+    };
+
+    db.todos.push(newTodo);
+    return newTodo;
+  }
+});
